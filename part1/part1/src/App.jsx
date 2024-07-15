07b3f9178bcccb40e83cc7f24313e7121700351b
watteja <mateyast@gmail.com>
2024-07-15 14:55:28 +0200

    Exercise 1.2: Course Information, step 2

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -2,19 +2,21 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
 const Content = (props) => {
   return (
-    <>
-      <p>
-        {props.parts[0]} {props.exercises[0]}
-      </p>
-      <p>
-        {props.parts[1]} {props.exercises[1]}
-      </p>
-      <p>
-        {props.parts[2]} {props.exercises[2]}
-      </p>
-    </>
+    <div>
+      <Part part={props.parts[0]} exercises={props.exercises[0]} />
+      <Part part={props.parts[1]} exercises={props.exercises[1]} />
+      <Part part={props.parts[2]} exercises={props.exercises[2]} />
+    </div>
   );
 };
 
