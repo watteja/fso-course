501686ef3f26fc252a857c775061ee0b910f3fa6
watteja <mateyast@gmail.com>
2024-08-01 15:13:09 +0200

    Exercise 4.4: Helper Functions and Unit Tests, step 2

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,16 @@
-const dummy = (blogs) => {
-  // ...
+const dummy = (_blogs) => {
   return 1;
+};
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes;
+  };
+
+  return blogs.reduce(reducer, 0);
 };
 
 module.exports = {
   dummy,
+  totalLikes,
 };
