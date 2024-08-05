6ec09dc88709619f15c2207e3923bafb5000c3e5
watteja <mateyast@gmail.com>
2024-08-05 13:01:21 +0200

    Exercise 4.9: Blog List Tests, step 2

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -1,10 +1,37 @@
-const { test, after } = require("node:test");
+const { test, after, beforeEach } = require("node:test");
 const assert = require("node:assert");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
-
 const api = supertest(app);
+
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+  {
+    title: "This is a random title",
+    author: "John Doe",
+    url: "https://www.example.com/clever-blogpost",
+    likes: 18,
+  },
+  {
+    title: "5 ways to make your code more readable",
+    author: "Lizzy McBlogsalot",
+    url: "example.com/reader-friendly-0245882",
+    likes: 22,
+  },
+];
+
+beforeEach(async () => {
+  // clear the data from the test version of database
+  await Blog.deleteMany({});
+
+  // insert initial data
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
 
 test("blogs are returned as json", async () => {
   await api
@@ -16,6 +43,12 @@ test("blogs are returned as json", async () => {
 test("there are two blogs in the list", async () => {
   const response = await api.get("/api/blogs");
   assert.strictEqual(response.body.length, 2);
+});
+
+// verify that the unique identifier property of the blog posts is named id
+test.only("the unique identifier property of the blog posts is named id", async () => {
+  const response = await api.get("/api/blogs");
+  assert(response.body[0].id);
 });
 
 after(async () => {
