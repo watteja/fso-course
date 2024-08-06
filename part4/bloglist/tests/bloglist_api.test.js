4192a9627187941c5cf4005a7fe03e73dbac2fd0
watteja <mateyast@gmail.com>
2024-08-06 10:22:55 +0200

    Refactor bloglist tests.

--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -7,36 +7,28 @@ const api = supertest(app);
 
 const Blog = require("../models/blog");
 
-// Wrapping the tests in a describe block, since after hook doesn't work
-// reliably otherwise.
-describe("tests", async () => {
-  after(async () => {
-    await mongoose.connection.close();
-  });
+const initialBlogs = [
+  {
+    title: "This is a random title",
+    author: "John Doe",
+    url: "https://www.example.com/clever-blogpost",
+    likes: 18,
+  },
+  {
+    title: "5 ways to make your code more readable",
+    author: "Lizzy McBlogsalot",
+    url: "example.com/reader-friendly-0245882",
+    likes: 22,
+  },
+];
 
-  const initialBlogs = [
-    {
-      title: "This is a random title",
-      author: "John Doe",
-      url: "https://www.example.com/clever-blogpost",
-      likes: 18,
-    },
-    {
-      title: "5 ways to make your code more readable",
-      author: "Lizzy McBlogsalot",
-      url: "example.com/reader-friendly-0245882",
-      likes: 22,
-    },
-  ];
-
+describe("when there are some initial notes saved", async () => {
   beforeEach(async () => {
     // clear the data from the test version of database
     await Blog.deleteMany({});
-    console.log("clear bloglist");
 
     // insert initial data
     await Blog.insertMany(initialBlogs);
-    console.log("inserted initial data");
   });
 
   test("blogs are returned as json", async () => {
@@ -46,9 +38,9 @@ describe("tests", async () => {
       .expect("Content-Type", /application\/json/);
   });
 
-  test("there are two blogs in the list", async () => {
+  test("all blogs are returned", async () => {
     const response = await api.get("/api/blogs");
-    assert.strictEqual(response.body.length, 2);
+    assert.strictEqual(response.body.length, initialBlogs.length);
   });
 
   test("the unique identifier property of the blog posts is named id", async () => {
@@ -56,54 +48,56 @@ describe("tests", async () => {
     assert(response.body[0].id);
   });
 
-  test("a valid blog can be added", async () => {
-    const newBlog = {
-      title: "Blogpost for testing the POST method",
-      author: "Jane Doe",
-      url: "https://www.example.com/testing-post-method",
-    };
+  describe("addition of a new blogpost", async () => {
+    test("succeeds with valid data", async () => {
+      const newBlog = {
+        title: "Blogpost for testing the POST method",
+        author: "Jane Doe",
+        url: "https://www.example.com/testing-post-method",
+      };
 
-    await api
-      .post("/api/blogs")
-      .send(newBlog)
-      .expect(201)
-      .expect("Content-Type", /application\/json/);
+      await api
+        .post("/api/blogs")
+        .send(newBlog)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
 
-    const response = await api.get("/api/blogs");
-    assert.strictEqual(response.body.length, initialBlogs.length + 1);
+      const response = await api.get("/api/blogs");
+      assert.strictEqual(response.body.length, initialBlogs.length + 1);
 
-    const blogPostTitles = response.body.map((blog) => blog.title);
-    assert(blogPostTitles.includes("Blogpost for testing the POST method"));
+      const blogPostTitles = response.body.map((blog) => blog.title);
+      assert(blogPostTitles.includes("Blogpost for testing the POST method"));
+    });
+
+    test("if the likes property is missing from the request, it will default to 0", async () => {
+      const blogWithMissingLikes = {
+        title: "Blogpost without any registered likes",
+        author: "Urkel the Unpopular",
+        url: "https://www.example.com/nobody-likes-me",
+      };
+
+      const response = await api.post("/api/blogs").send(blogWithMissingLikes);
+      assert.strictEqual(response.body.likes, 0);
+    });
+
+    test("if the blogpost is missing title or url, respond with 400 Bad Request", async () => {
+      const blogWithMissingTitle = {
+        author: "Frank Forgetful",
+        url: "example.com/not-even-a-title",
+      };
+      const blogWithMissingUrl = {
+        title: "Blogpost with missing URL",
+        author: "Frank Forgetful",
+      };
+
+      let response = await api.post("/api/blogs").send(blogWithMissingTitle);
+      assert.strictEqual(response.status, 400);
+      response = await api.post("/api/blogs").send(blogWithMissingUrl);
+      assert.strictEqual(response.status, 400);
+    });
   });
 
-  test("if the likes property is missing from the request, it will default to 0", async () => {
-    const blogWithMissingLikes = {
-      title: "Blogpost without any registered likes",
-      author: "Urkel the Unpopular",
-      url: "https://www.example.com/nobody-likes-me",
-    };
-
-    const response = await api.post("/api/blogs").send(blogWithMissingLikes);
-    assert.strictEqual(response.body.likes, 0);
-  });
-
-  test("if the blog is missing title or url, respond with 400 Bad Request", async () => {
-    const blogWithMissingTitle = {
-      author: "Frank Forgetful",
-      url: "example.com/not-even-a-title",
-    };
-    const blogWithMissingUrl = {
-      title: "Blogpost with missing URL",
-      author: "Frank Forgetful",
-    };
-
-    let response = await api.post("/api/blogs").send(blogWithMissingTitle);
-    assert.strictEqual(response.status, 400);
-    response = await api.post("/api/blogs").send(blogWithMissingUrl);
-    assert.strictEqual(response.status, 400);
-  });
-
-  test("deletion of a blog", async () => {
+  test("deletion of a blogpost", async () => {
     const initialResponse = await api.get("/api/blogs");
     const blogToDelete = initialResponse.body[0];
     await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
@@ -125,5 +119,10 @@ describe("tests", async () => {
       .put(`/api/blogs/${blogToUpdate.id}`)
       .send(updatedBlog); // just like for POST, we send JSON data
     assert.strictEqual(response.body.likes, blogToUpdate.likes + 10);
+  });
+
+  // Test runner doesn't work reliably if we register after hook outside of the describe block
+  after(async () => {
+    await mongoose.connection.close();
   });
 });
