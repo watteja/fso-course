4e56fcf427dbe7d336cde5e0124a0f222397ffc5
watteja <mateyast@gmail.com>
2024-08-16 14:36:17 +0200

    Refactor forms into separate components.

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -3,16 +3,13 @@ import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 import Notification from "./components/Notification";
+import LoginForm from "./components/LoginForm";
+import BlogForm from "./components/BlogForm";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [url, setUrl] = useState("");
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
@@ -26,28 +23,16 @@ const App = () => {
     }
   }, []);
 
-  const addBlog = (event) => {
-    event.preventDefault();
-
-    const blogObject = { title, author, url };
-    blogService.create(blogObject).then((returnedBlog) => {
-      setBlogs(blogs.concat(returnedBlog));
-      setTitle("");
-      setAuthor("");
-      setUrl("");
-    });
+  const addBlog = (newBlog) => {
+    setBlogs(blogs.concat(newBlog));
   };
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
-
+  const handleLogin = async (username, password) => {
     try {
       const user = await loginService.login({ username, password });
       window.localStorage.setItem("loggedBloglistUser", JSON.stringify(user));
       blogService.setToken(user.token);
       setUser(user);
-      setUsername("");
-      setPassword("");
     } catch {
       setErrorMessage("Wrong credentials");
       setTimeout(() => {
@@ -62,69 +47,12 @@ const App = () => {
     blogService.setToken(null); // just in case
   };
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>
-  );
-
-  const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <div>
-        title:
-        <input
-          type="text"
-          value={title}
-          name="Title"
-          onChange={({ target }) => setTitle(target.value)}
-        />
-      </div>
-      <div>
-        author:
-        <input
-          type="text"
-          value={author}
-          name="Author"
-          onChange={({ target }) => setAuthor(target.value)}
-        />
-      </div>
-      <div>
-        url:
-        <input
-          type="text"
-          value={url}
-          name="Url"
-          onChange={({ target }) => setUrl(target.value)}
-        />
-      </div>
-      <button type="submit">create</button>
-    </form>
-  );
-
   if (user === null) {
     return (
       <div>
         <h2>Log in to application</h2>
         <Notification message={errorMessage} />
-        {loginForm()}
+        <LoginForm onLogin={handleLogin} />
       </div>
     );
   }
@@ -136,7 +64,7 @@ const App = () => {
         {user.name} logged in<button onClick={handleLogout}>logout</button>
       </p>
 
-      {blogForm()}
+      <BlogForm onAddBlog={addBlog} />
 
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
---
+++ part5/bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import blogService from "../services/blogs";
+
+const BlogForm = ({ onAddBlog }) => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleAddBlog = (event) => {
+    event.preventDefault();
+
+    const blogObject = { title, author, url };
+    blogService.create(blogObject).then((returnedBlog) => {
+      onAddBlog(returnedBlog);
+      setTitle("");
+      setAuthor("");
+      setUrl("");
+    });
+  };
+
+  return (
+    <form onSubmit={handleAddBlog}>
+      <div>
+        title:
+        <input
+          type="text"
+          value={title}
+          name="Title"
+          onChange={({ target }) => setTitle(target.value)}
+        />
+      </div>
+      <div>
+        author:
+        <input
+          type="text"
+          value={author}
+          name="Author"
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+      </div>
+      <div>
+        url:
+        <input
+          type="text"
+          value={url}
+          name="Url"
+          onChange={({ target }) => setUrl(target.value)}
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  );
+};
+
+export default BlogForm;
