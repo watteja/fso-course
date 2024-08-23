c1ab384839dc0c615579bd8d226e974c82e2755a
watteja <mateyast@gmail.com>
2024-08-23 21:17:23 +0200

    Exercise 5.10: Blog List Frontend, step 10

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -69,9 +69,11 @@ const App = () => {
         <BlogForm onAddBlog={addBlog} />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} onUpdate={updateBlog} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} onUpdate={updateBlog} />
+        ))}
     </div>
   );
 };
