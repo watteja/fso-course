88eaa87b9f32ffe2651377ec0244727664c77b00
watteja <mateyast@gmail.com>
2024-09-23 13:33:12 +0200

    Exercise 5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
-import { expect } from "vitest";
+import { expect, test } from "vitest";
 
 test("at start renders only blog title and author", () => {
   const blog = {
     title: "Test blog for testing with react-testing-library",
-    author: "Jane Doe",
+    author: "John Tester",
     url: "https://example.com/test-blog",
     likes: 2,
   };
@@ -16,4 +17,26 @@ test("at start renders only blog title and author", () => {
   expect(screen.queryByText(blog.author)).toBeDefined();
   expect(screen.queryByText(blog.url)).toBeNull();
   expect(screen.queryByText(`likes ${blog.likes}`)).toBeNull();
+});
+
+test("url and likes are displayed when view button is clicked", async () => {
+  const user = {
+    id: "test-id",
+    name: "Test User",
+  };
+
+  const blog = {
+    title: "Test blog for testing with react-testing-library",
+    author: "John Tester",
+    url: "https://example.com/test-blog",
+    likes: 2,
+    user,
+  };
+
+  render(<Blog blog={blog} user={user} />);
+  const button = screen.getByText("view");
+  await userEvent.click(button);
+
+  expect(screen.queryByText(blog.url)).toBeDefined();
+  expect(screen.queryByText(`likes ${blog.likes}`)).toBeDefined();
 });
