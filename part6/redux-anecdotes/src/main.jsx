f940a77f5fbcc18574ec128adbd19655b3e0f3c5
watteja <mateyast@gmail.com>
2024-10-25 11:18:12 +0200

    Exercise 6.16 Anecdotes and the Backend, step 3

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import store from "./store";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice, current } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
@@ -21,4 +22,12 @@ const anecdoteSlice = createSlice({
 });
 
 export const { voteFor, createAnecdote, setAnecdotes } = anecdoteSlice.actions;
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
+
 export default anecdoteSlice.reducer;
