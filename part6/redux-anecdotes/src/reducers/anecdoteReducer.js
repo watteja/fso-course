beb725e6aee3af04349003989a7f67b932186d14
watteja <mateyast@gmail.com>
2024-10-25 10:37:34 +0200

    Exercise 6.15 Anecdotes and the Backend, step 2

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,15 +1,5 @@
 import { createSlice, current } from "@reduxjs/toolkit";
 
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
 const anecdoteSlice = createSlice({
   name: "anecdotes",
   initialState: [],
@@ -22,8 +12,7 @@ const anecdoteSlice = createSlice({
       console.log("state after", current(state));
     },
     createAnecdote(state, action) {
-      const newAnecdote = asObject(action.payload);
-      state.push(newAnecdote); // Immutable due to Immer
+      state.push(action.payload); // Immutable due to Immer
     },
     setAnecdotes(_state, action) {
       return action.payload;
