ad46b41b4323b9943fecf7baefb7fe9e73bd2f59
watteja <mateyast@gmail.com>
2024-11-04 15:19:33 +0100

    Exercise 7.5: Anecdotes and Hooks step 2

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -102,6 +102,12 @@ const CreateNew = (props) => {
     navigate("/");
   };
 
+  const handleReset = () => {
+    content.reset();
+    author.reset();
+    info.reset();
+  };
+
   return (
     <div>
       <h2>create a new anecdote</h2>
@@ -118,7 +124,10 @@ const CreateNew = (props) => {
           url for more info
           <input {...info} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
       </form>
     </div>
   );
@@ -184,10 +193,12 @@ const App = () => {
           path="/anecdotes"
           element={<AnecdoteList anecdotes={anecdotes} />}
         />
-        <Route
-          path="/anecdotes/:id"
-          element={<Anecdote anecdote={anecdote} />}
-        />
+        {anecdote && (
+          <Route
+            path="/anecdotes/:id"
+            element={<Anecdote anecdote={anecdote} />}
+          />
+        )}
         <Route path="/create" element={<CreateNew addNew={addNew} />} />
         <Route path="/about" element={<About />} />
       </Routes>
